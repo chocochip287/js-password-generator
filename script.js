7296cd3b183c6ef5f72e21abbe74b827fb0752a9
Matt <39172048+magiccreb@users.noreply.github.com>
2022-09-29 09:02:44 -0400

    set up functions for determining password criteria prior to generation

--- script.js
+++ script.js
@@ -20,6 +20,13 @@ THEN the password is either displayed in an alert or written to the page
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// declaring password criteria variables
+var pwLength = 0;
+var pwLowercase = true;
+var pwUppercase = true;
+var pwNumeric = true;
+var pwSpecial = true;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -31,5 +38,37 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button to establish password criteria then generate a password
+
+generateBtn.addEventListener("click", lengthPrompt);
+
+// Password criteria prompts
+
+// prompts the user to enter a value to determine password length
+function lengthPrompt() {
+  pwLength = prompt("Please enter a number between 8 and 128 to determine your password's length.", "[enter your number here]");
+  while (pwLength > 128 || pwLength < 8) {
+    pwLength = prompt("You must enter a number between 8 and 128.", "[please enter a valid number]");
+  }
+  lowercasePrompt();
+}
+// prompts the user to choose whether or not to allow lowercase characters
+function lowercasePrompt() {
+  pwLowercase = confirm("Inlcude lowercase characters in your password? OK = yes, Cancel = no.");
+  uppercasePrompt();
+}
+// prompts the user to choose whether or not to allow uppercase characters
+function uppercasePrompt() {
+  pwLowercase = confirm("Inlcude lowercase characters in your password? OK = yes, Cancel = no.");
+  numPrompt();
+}
+// prompts the user to choose whether or not to allow numbers
+function numPrompt() {
+  pwNumeric = confirm("Inlcude numbers in your password? OK = yes, Cancel = no.");
+  specPrompt();
+}
+// prompts the user to choose whether or not to allow special characters
+function specPrompt() {
+  pwSpecial = confirm("Inlcude special characters in your password? OK = yes, Cancel = no.");
+  writePassword();
+}
