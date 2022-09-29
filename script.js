ad49987b86c554e79c873b436368873dc2b3c00d
Matt <39172048+magiccreb@users.noreply.github.com>
2022-09-29 16:49:49 -0400

    assignment.md removed. README updated.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*
 9/29/2022:
 No big remaining to-dos as of this writing. See the end of the file for a future experiment based on the 
-starter files.
+starter files. Original assignment also placed at the end.
 */
 
 // Assignment Code
@@ -201,4 +201,21 @@ function writePassword() {
   }
   
   passwordText.value = password;
+
+  Assignment:
+  GIVEN I need a new, secure password
+  WHEN I click the button to generate a password
+  THEN I am presented with a series of prompts for password criteria
+  WHEN prompted for password criteria
+  THEN I select which criteria to include in the password
+  WHEN prompted for the length of the password
+  THEN I choose a length of at least 8 characters and no more than 128 characters
+  WHEN asked for character types to include in the password
+  THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+  WHEN I answer each prompt
+  THEN my input should be validated and at least one character type should be selected
+  WHEN all prompts are answered
+  THEN a password is generated that matches the selected criteria
+  WHEN the password is generated
+  THEN the password is either displayed in an alert or written to the page
 */
