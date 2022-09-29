3c445ab37b98f321d6ad5e1cf521eaec0bd68935
Matt <39172048+magiccreb@users.noreply.github.com>
2022-09-29 15:29:36 -0400

    tidied up intro comment in script.js

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 /*
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
+9/29/2022:
+No big remaining to-dos as of this writing. See the end of the file for a future experiment based on the 
+starter files.
 */
 
 // Assignment Code
